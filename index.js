354776fae741d1fb957f275882d3b1e8ce9fceff
Vivek Jilla <vivekjilla7@gmail.com>
2023-12-27 15:42:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ const router = express.Router();
 
 // Hello World for index page
 app.get('/', function (req, res) {
-    return res.send("Hello World 2!");
+    return res.send("Hello World!");
 })
 
 app.get('/api', function (req, res) {
